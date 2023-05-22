9bb05f7bd7fa62b12428ec67545c7151170bebb6
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-05-22 19:07:54 -0300

    implementando unidad de trabajo

---
+++ BlogAccesoDatos/Data/Repository/IRepository/IContenedorTrabajo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.AccesoDatos.Data.Repository.IRepository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        ICategoriaRepository Categoria { get; }
+        //Aquí se deben ir agregando los diferentes repositorios
+
+        void Save();
+
+    }
+}
