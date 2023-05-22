36ef5f244ff46eff211a5411ed4fda695cf35d3c
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-05-22 18:57:15 -0300

    Repositorio Categoria

---
+++ BlogAccesoDatos/Data/Repository/CategoriaRepository.cs
@@ -0,0 +1,42 @@
+﻿using Blog.AccesoDatos.Data.Repository.IRepository;
+using Blog.Data;
+using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Blog.AccesoDatos.Data.Repository
+{
+    internal class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriaRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetListaCategorias()
+        {
+            return _db.Categoria.Select(i => new SelectListItem()
+            {
+                Text = i.Nombre,
+                Value = i.Id.ToString()
+            }
+            );
+        }
+
+        public void Update(Categoria categoria)
+        {
+            var objDesdeDb = _db.Categoria.FirstOrDefault(s => s.Id == categoria.Id);
+            objDesdeDb.Nombre = categoria.Nombre;
+            objDesdeDb.Orden = categoria.Orden;
+
+            _db.SaveChanges();
+
+        }
+    }
+}
