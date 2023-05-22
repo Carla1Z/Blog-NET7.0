f149401935366fe6db800e622541fa1e3fa7285c
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-05-22 19:17:53 -0300

    controlador categorias

---
+++ Blog/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,39 @@
+﻿using Blog.AccesoDatos.Data.Repository.IRepository;
+using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo, ApplicationDbContext context)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        #region Llamadas a la API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Opcion 1
+            return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+        }
+
+        #endregion
+    }
+}
